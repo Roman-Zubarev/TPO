e33aac2fc9f8e952090366b11a6f7af6acb11b12
Роман Зубарев <rvz031003@gmail.com>
2023-10-16 20:12:32 +0300

    7lab

---
+++ 7lab/7lab/Program.cs
@@ -0,0 +1,22 @@
+﻿using Solver;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var result = QuadraticSolver.Solve(2, 2, 2);
+        Console.WriteLine(result[0]);
+       // Console.WriteLine(result[1]);
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
---
+++ 7lab/Solver.Tests/UnitTest1.cs
@@ -0,0 +1,47 @@
+using Xunit.Sdk;
+using Solver;
+namespace Solver.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+       
+        public void GeneralTest(double a, double b, double c, params double[] expectedResult)
+        {
+            var result = QuadraticSolver.Solve(a,b,c);
+
+            Assert.AreEqual(expectedResult.Length, result.Length);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(expectedResult[i], result[i]);
+            }
+        }
+        [TestMethod]
+        public void UsualTest()
+        {
+            GeneralTest(1,5,4,-1,-4);
+        }
+        [TestMethod]
+        public void UsualTest2()
+        {
+            GeneralTest(1, -3, 2, 2, 1);
+        }
+        [TestMethod]
+        public void NegativeDisctiminant()
+        {
+            GeneralTest(1,1,1);
+        }
+        [TestMethod]
+        public void NegativeDisctiminant2()
+        {
+            GeneralTest(2, 2, 2);
+        }
+
+        [TestMethod]
+        public void ZeroDisciminant()
+        {
+            GeneralTest(1, 2, 1, -1);
+        }
+    }
+}
